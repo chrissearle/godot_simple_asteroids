be0f08d3a870abb0fbdb1983a9a2b5853e91d09d
Chris Searle <chris@chrissearle.org>
2022-11-08 17:13:50 +0100

    Add boom sound

--- World.gd
+++ World.gd
@@ -7,7 +7,10 @@ onready var size = get_viewport().size
 onready var player = $Player
 onready var fire_sound_player = $FireSoundPlayer
 onready var kill_sound_player = $KillSoundPlayer
+onready var boom_sound_player = $BoomSoundPlayer
 onready var label_container = $LabelContainer
+
+var asteroid_count = 0
 
 func _ready():
 	label_container.visible = false
@@ -22,6 +25,9 @@ func _ready():
 		build_asteroid(true, true, null)
 
 func _process(_delta):
+	if asteroid_count == 0:
+		label_container.visible = true
+	
 	if label_container.visible and Input.is_action_pressed("ui_accept"):
 		get_tree().reload_current_scene()
 
@@ -34,15 +40,17 @@ func kill_player():
 	player.queue_free()
 
 func hit(asteroid_area, pos, large):
-	# Play explode
+	boom_sound_player.play()
 	if large:
 		for _i in range(3):
 			build_asteroid(false, false, pos)
 
+	asteroid_count -= 1
 	asteroid_area.queue_free()
 
 func build_asteroid(large, random_position, fixed_position):
 	var asteroid_instance = asteroid.instance()
+	asteroid_count += 1
 	add_child(asteroid_instance)
 	if random_position:
 		asteroid_instance.global_position = Vector2(size.x * randf(), size.y * randf())
